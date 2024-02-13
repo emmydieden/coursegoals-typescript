3de85ac669f7102c0adb6602b4db00bca054d917
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2024-02-13 08:39:07 +0100

    managing state with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
+import Header from "./components/Header";
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/goals.jpg";
 
-
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth!",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
   return (
     <main>
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up</p>
-        </CourseGoal>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your course goals</h1>
+      </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
+    <ul>
+      {goals.map((goal) => (
+        <li key={goal.id}>
+          <CourseGoal title={goal.title}>
+            <p>{goal.description}</p>
+          </CourseGoal>
+        </li>
+      ))}
+      </ul>
     </main>
   );
 }
